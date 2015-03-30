4469e46fb5d5fba75c8dd97e6ec56a7bac789c17
Demetris Manikas <demetris.manikas@gmail.com>
2015-03-30 17:42:09 +0300

    Removed AES implementation for until it is used

--- src/android/SecureStorage.java
+++ src/android/SecureStorage.java
@@ -60,40 +60,6 @@ public class SecureStorage extends CordovaPlugin {
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
                     try {
-                        Cipher encKeyCipher = RSA.createCipher(Cipher.ENCRYPT_MODE, ALIAS);
-                        JSONObject result = AES.encrypt(encryptMe.getBytes(), encKeyCipher);
-                        callbackContext.success(result);
-                    } catch (Exception e) {
-                        Log.e(TAG, "Encrypt failed :", e);
-                        callbackContext.error(e.getMessage());
-                    }
-                }
-            });
-            return true;
-        }
-        if ("decrypt".equals(action)) {
-            final byte[] encKey = args.getArrayBuffer(0);
-            final byte[] iv = args.getArrayBuffer(1);
-            final byte[] decryptMe = args.getArrayBuffer(2);
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    try {
-                        byte[] key = RSA.decrypt(encKey, ALIAS);
-                        String decrypted = new String(AES.decrypt(decryptMe, key, iv));
-                        callbackContext.success(decrypted);
-                    } catch (Exception e) {
-                        Log.e(TAG, "Decrypt failed :", e);
-                        callbackContext.error(e.getMessage());
-                    }
-                }
-            });
-            return true;
-        }
-        if ("encrypt_rsa".equals(action)) {
-            final String encryptMe = args.getString(0);
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    try {
                         byte[] encrypted = RSA.encrypt(encryptMe.getBytes(), ALIAS);
                         callbackContext.success(Base64.encodeToString(encrypted, Base64.DEFAULT));
                     } catch (Exception e) {
@@ -104,7 +70,7 @@ public class SecureStorage extends CordovaPlugin {
             });
             return true;
         }
-        if ("decrypt_rsa".equals(action)) {
+        if ("decrypt".equals(action)) {
             final byte[] decryptMe = args.getArrayBuffer(0);
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
