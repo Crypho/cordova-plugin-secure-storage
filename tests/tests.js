519d864d27e14d70f6a31776f33386e9bb23e714
Yiorgis Gozadinos <ggozad@crypho.com>
2015-04-09 14:51:49 +0200

    add tests for the rest of the API

--- tests/tests.js
+++ tests/tests.js
@@ -25,8 +25,74 @@ exports.defineAutoTests = function() {
             spyOn(handlers, 'errorHandler');
 
             ss = new cordova.plugins.SecureStorage(handlers.successHandler, handlers.errorHandler, 'testing');
-
         });
+
+        it('should be able to set a key/value', function (done) {
+            spyOn(handlers, 'successHandler').and.callFake(function (res) {
+                expect(res).toEqual('foo');
+                expect(handlers.errorHandler).not.toHaveBeenCalled();
+                done();
+            });
+            spyOn(handlers, 'errorHandler');
+
+            ss = new cordova.plugins.SecureStorage(function (res) {
+                ss.set(handlers.successHandler, handlers.errorHandler, 'foo', 'bar');
+            }, handlers.errorHandler, 'testing');
+        });
+
+        it('should be able to get a key/value', function (done) {
+            spyOn(handlers, 'successHandler').and.callFake(function (res) {
+                expect(res).toEqual('bar');
+                expect(handlers.errorHandler).not.toHaveBeenCalled();
+                done();
+            });
+            spyOn(handlers, 'errorHandler');
+
+            ss = new cordova.plugins.SecureStorage(function (res) {
+                ss.set(function () {
+                    ss.get(handlers.successHandler, handlers.errorHandler, 'foo');
+                }, handlers.errorHandler, 'foo', 'bar');
+            }, handlers.errorHandler, 'testing');
+        });
+
+        it('should be able to get a key/value that was set before', function (done) {
+            spyOn(handlers, 'successHandler').and.callFake(function (res) {
+                expect(res).toEqual('bar');
+                expect(handlers.errorHandler).not.toHaveBeenCalled();
+                done();
+            });
+            spyOn(handlers, 'errorHandler');
+
+            ss = new cordova.plugins.SecureStorage(function (res) {
+                ss.get(handlers.successHandler, handlers.errorHandler, 'foo');
+            }, handlers.errorHandler, 'testing');
+        });
+
+        it('should call the error handler when getting a key that does not exist', function (done) {
+            spyOn(handlers, 'errorHandler').and.callFake(function (res) {
+                expect(handlers.successHandler).not.toHaveBeenCalled();
+                done();
+            });
+            spyOn(handlers, 'successHandler');
+
+            ss = new cordova.plugins.SecureStorage(function (res) {
+                ss.get(handlers.successHandler, handlers.errorHandler, 'nofoo');
+            }, handlers.errorHandler, 'testing');
+        });
+
+        it('should be able to remove a key/value', function (done) {
+            spyOn(handlers, 'successHandler').and.callFake(function (res) {
+                expect(res).toEqual('foo');
+                expect(handlers.errorHandler).not.toHaveBeenCalled();
+                done();
+            });
+            spyOn(handlers, 'errorHandler');
+
+            ss = new cordova.plugins.SecureStorage(function (res) {
+                ss.remove(handlers.successHandler, handlers.errorHandler, 'foo');
+            }, handlers.errorHandler, 'testing');
+        });
+
 
     });
 };
