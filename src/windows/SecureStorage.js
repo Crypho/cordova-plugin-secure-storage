ae6a2bd4ad1bcdf3521891725f64a80361fd0472
Yiorgis Gozadinos <ggozad@crypho.com>
2016-10-26 09:19:50 +0200

    Catch no credential found exceptions in windows

--- src/windows/SecureStorage.js
+++ src/windows/SecureStorage.js
@@ -1,4 +1,5 @@
 var SecureStorageProxy = {
+
     get: function (win, fail, args) {
         try {
             var service = args[0];
@@ -12,6 +13,7 @@ var SecureStorageProxy = {
             fail('Failure in SecureStorage.get() - ' + e.message);
         }
     },
+
     set: function (win, fail, args) {
         try {
             var service = args[0];
@@ -31,6 +33,7 @@ var SecureStorageProxy = {
             fail('Failure in SecureStorage.set() - ' + e.message);
         }
     },
+
     remove: function (win, fail, args) {
         try {
             var service = args[0];
@@ -48,12 +51,18 @@ var SecureStorageProxy = {
             fail('Failure in SecureStorage.remove() - ' + e.message);
         }
     },
+
     keys: function (win, fail, args) {
         try {
             var service = args[0];
-
             var vault = new Windows.Security.Credentials.PasswordVault();
-            var passwordCredentials = vault.findAllByResource(service) || [];
+            var passwordCredentials;
+
+            try {
+                passwordCredentials = vault.findAllByResource(service);
+            } catch (e) {
+                passwordCredentials = [];
+            }
             passwordCredentials = passwordCredentials.map(function (passwordCredential) {
                 return passwordCredential.userName;
             });
@@ -63,12 +72,18 @@ var SecureStorageProxy = {
             fail('Failure in SecureStorage.keys() - ' + e.message);
         }
     },
+
     clear: function (win, fail, args) {
         try {
             var service = args[0];
-
             var vault = new Windows.Security.Credentials.PasswordVault();
-            var passwordCredentials = vault.findAllByResource(service) || [];
+            var passwordCredentials;
+
+            try {
+                passwordCredentials = vault.findAllByResource(service);
+            } catch (e) {
+                passwordCredentials = [];
+            }
             passwordCredentials.forEach(function (passwordCredential) {
                 vault.remove(passwordCredential);
             });
